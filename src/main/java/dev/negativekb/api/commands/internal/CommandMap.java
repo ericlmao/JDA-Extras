a6395d5b872d2cf8a6ea32dc7c45ab90c7224fd7
Negative <60104846+NegativeKB@users.noreply.github.com>
2021-12-15 23:49:06 -0330

    Add more notes

--- src/main/java/dev/negativekb/api/commands/internal/CommandMap.java
+++ src/main/java/dev/negativekb/api/commands/internal/CommandMap.java
@@ -1,15 +1,33 @@
 package dev.negativekb.api.commands.internal;
 
 import dev.negativekb.api.commands.Command;
+import net.dv8tion.jda.api.entities.Guild;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.HashMap;
 
+/**
+ * Command Map Module
+ *
+ * This is responsible for handling and registering all
+ * commands using the {@link Command} abstract class
+ */
 public interface CommandMap {
 
+    /**
+     * Register a Global Command
+     * @param name {@link Command} Name
+     * @param command {@link Command} instance
+     */
     void registerGlobalCommand(@NotNull String name, @NotNull Command command);
 
+    /**
+     * Register a {@link Command} to a {@link Guild}
+     * @param serverID ID ({@link String}) of the {@link Guild}
+     * @param name {@link Command} name
+     * @param command {@link Command} instance
+     */
     void registerServerCommand(@NotNull String serverID, @NotNull String name, @NotNull Command command);
 
     @NotNull
