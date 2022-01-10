d573ff2146ec139702ab2af3fab495c564af6645
Negative <60104846+NegativeKB@users.noreply.github.com>
2022-01-09 23:51:11 -0330

    Add Command Cooldowns

--- src/main/java/dev/negativekb/api/commands/Command.java
+++ src/main/java/dev/negativekb/api/commands/Command.java
@@ -1,8 +1,11 @@
 package dev.negativekb.api.commands;
 
 import dev.negativekb.api.commands.exception.InvalidCommandInfoException;
+import dev.negativekb.api.provider.CommandCooldownManagerProvider;
+import dev.negativekb.api.util.TimeUtil;
 import lombok.Getter;
 import lombok.Setter;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
@@ -24,10 +27,15 @@ public abstract class Command {
     private Consumer<CommandData> data;
     @Getter
     private final ArrayList<SubCommand> subCommands = new ArrayList<>();
+    @Setter
+    private int cooldownInSeconds = 0;
+    private final CommandCooldownManager cooldownManager;
 
     public Command() {
         if (!getClass().isAnnotationPresent(CommandInfo.class))
             throw new InvalidCommandInfoException();
+
+        cooldownManager = CommandCooldownManagerProvider.getInstance();
 
         CommandInfo info = getClass().getAnnotation(CommandInfo.class);
         this.name = info.name();
@@ -52,6 +60,23 @@ public abstract class Command {
 
             first.ifPresent(subCommand -> subCommand.runCommand(event));
             return;
+        }
+
+        if (cooldownInSeconds != 0) {
+            User user = event.getUser();
+            String id = user.getId();
+
+            if (cooldownManager.checkCooldown(id, this)) {
+                // Reply with cooldown message
+                long cooldown = cooldownManager.getCooldown(id, this);
+                String format = TimeUtil.format(cooldown, System.currentTimeMillis());
+                event.reply("You cannot use this command for another " +
+                        "**" + format + "**!").setEphemeral(true).queue();
+                return;
+            }
+
+            long cooldownInMills = cooldownInSeconds * 1000L;
+            cooldownManager.addCooldown(id, this, cooldownInMills);
         }
 
         onCommand(event);
